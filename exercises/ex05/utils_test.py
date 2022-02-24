61502438a17398add6a6f116b2aa558faa485aea
Itzel Montes Ruiz <itzelmr@live.unc.edu>
2022-02-23 20:27:03 -0500

    finished

---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,23 @@
+""" __author__: 730335383"""
+
+
+from utils import only_evens
+from utils import sub
+from utils import concat 
+
+
+def test_only_evens():
+    assert only_evens([1, 2, 3]) == [2]
+
+
+def test_sub():
+    assert sub([10, 20, 30, 40], -1, 6) == [10, 20, 30]
+
+
+def test_concat():
+    assert concat([1, 2, 3], [4, 5, 6])
+
+
+concat
+sub
+only_evens
